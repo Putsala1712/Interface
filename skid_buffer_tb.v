20f43361033ad9ac17cf39e1206f47616dde410d
Pooja Sri Putsala <98636476+Putsala1712@users.noreply.github.com>
2025-09-10 14:07:52 +0530

    skid_buffer_tb.v

--- skid_buffer_tb.v
+++ skid_buffer_tb.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09.09.2025 12:39:22
-// Design Name: 
-// Module Name: skid_buffer_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module skid_buffer_tb();
 parameter WIDTH=32;
