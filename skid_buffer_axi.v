90a2a9a0eb7666178395cd2749c0fe096e4e6422
Pooja Sri Putsala <98636476+Putsala1712@users.noreply.github.com>
2025-09-10 14:06:14 +0530

    skid_buffer_axi.v

--- skid_buffer_axi.v
+++ skid_buffer_axi.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09.09.2025 08:32:06
-// Design Name: 
-// Module Name: skid_buffer_axi
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module skid_buffer_axi #(parameter WIDTH=32)
  ( // Global signals
