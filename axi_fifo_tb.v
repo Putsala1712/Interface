ec88833e1eb9745d628cdba2420ee7014b8a46d9
Pooja Sri Putsala <98636476+Putsala1712@users.noreply.github.com>
2025-09-22 12:58:21 +0530

    Update axi_fifo_tb.v

--- axi_fifo_tb.v
+++ axi_fifo_tb.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 19.09.2025 15:24:53
-// Design Name: 
-// Module Name: axi_fifo_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module axi_fifo_tb();
 parameter DATA_WIDTH = 32;
@@ -110,9 +90,7 @@ end
 
 join
 
-
 #50;
 $finish;
-end
-   
+end   
 endmodule
